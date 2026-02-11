671526c1e0d3f17c8c89547cb015543b2af1613f
Chris Bell <bellcn@hendrix.edu>
2026-02-10 18:34:16 -0600

    Added Images that Alternate and finished up button contents

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -11,27 +11,45 @@ $(document).ready(function() {
         } catch(error){
             console.error("Couldn't fetch quote")
         }
-    }   
-
-    function getStrangerThingsQuote(){
-
     }
+
+    async function getStrangerThingsQuote(){
+        try {
+            const response = await fetch("https://strangerthingsquotes.shadowdev.xyz/api/quotes")
+            if (!response.ok) {
+                throw new Error("Error with HTTP")
+            }
+            const data = await response.json();
+            return data[0].quote;
+        } catch(error){
+            console.error("Couldn't fetch quote")
+        }
+    }
+
 
      function chooseNumber(choice){
         if (choice < 0.5){
-            $("#quote").text(getKanyeQuote())
+            getKanyeQuote().then(function (quote) {
+                $("#quote").text(quote);
+            });
         }
-        else {
-            $("#quote").text("Stranger Things Quote")
+        if (choice >= 0.5){
+            getStrangerThingsQuote().then(function (quote){
+                $("#quote").text(quote);
+            });
         }
     }
 
     function updateScore(score){
         $("#score").html("Score:" + score)
+        $("#alternating-img").attr("src", correctImg)
     }
 
     let choice= Math.random();
     chooseNumber(choice);
+
+    const correctImg = "https://free-images.com/md/2c07/checkmark_bold_brush_green.jpg";
+    const wrongImg = "https://free-images.com/md/7b89/wrong_way_sign_road.jpg"
 
 
     var score=0;
@@ -39,6 +57,10 @@ $(document).ready(function() {
         if (choice < 0.5){
             score += 1;
             updateScore(score)
+            
+        }
+        else{
+            $("#alternating-img").attr("src", wrongImg)
         }
 
         choice=Math.random();
@@ -50,9 +72,11 @@ $(document).ready(function() {
             score += 1;
             updateScore(score)
         }
+        else{
+            $("#alternating-img").attr("src", wrongImg)
+        }
 
         choice=Math.random();
         chooseNumber(choice);
-
     });
 });
