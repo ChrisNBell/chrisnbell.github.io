116fab09c8889178a636469b0a16a1ce13362f94
Chris Bell <bellcn@hendrix.edu>
2026-03-30 15:10:48 -0500

    Project 9 Complete

--- csci340lab9/HendrixCollege/obj/Debug/net10.0/HendrixCollege.AssemblyInfo.cs
+++ csci340lab9/HendrixCollege/obj/Debug/net10.0/HendrixCollege.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HendrixCollege")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc4b8fb193d87fe8ad34c3209f794bb378ecb42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c1eaff202967e528019c71db8e2a978e6c1dd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HendrixCollege")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HendrixCollege")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
